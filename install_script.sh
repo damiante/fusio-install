c80d7552f60fb4f7244cc870b576c2760180d177
Damian Testa <damian@damiantesta.com>
2020-03-24 12:39:57 +1100

    fixed prompt inpipe to include administrator status

--- install_script.sh
+++ install_script.sh
@@ -230,7 +230,7 @@ install_fusio () {
     cd /var/www/fusio
     composer install
     printf "y" | php /var/www/fusio/bin/fusio install
-    printf "$fusio_user\n$fusio_password" | php /var/www/fusio/bin/fusio adduser
+    printf "1\n$fusio_user\n$fusio_password" | php /var/www/fusio/bin/fusio adduser
     sed -i "s/FUSIO_URL=.*/FUSIO_URL=\"http:\/\/127\.0\.0\.1\/fusio\/public\"/" /var/www/fusio/.env
     sed -i "s/FUSIO_DB_USER=.*/FUSIO_DB_USER=\"$fusio_db_user\"/" /var/www/fusio/.env
     sed -i "s/FUSIO_DB_PW=.*/FUSIO_DB_PW=\"$fusio_db_password\"/" /var/www/fusio/.env
